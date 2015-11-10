147faf98ff90c969d7d4afe1ed994babf385a01d
guangLess <bha.guang@gmail.com>
2015-11-10 14:05:35 -0500

    note changed

--- github-repo-starring/FISReposTableViewController.h
+++ github-repo-starring/FISReposTableViewController.h
@@ -10,6 +10,12 @@
  when call Internet related session, almost always finish with a completion block.
  __block NSString * x = @""; pass the varible to the block. 
  @"%@/user/starred/%@?client_id...  if put / before ? it wont work.
+ 
+check the repos with bool 
+then with each bool, do the api action. 
+ 
+ statusCode == 204, responsed successfully, it has nothing to do with PUT or Delete yet. 
+ 
  */
 
 #import <UIKit/UIKit.h>
